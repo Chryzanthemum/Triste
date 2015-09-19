6fc9b23b35cb8f7c9311b4d925d25785618dddd4
David Wu <davidwu@mit.edu>
2015-09-19 18:26:45 -0400

    Runs messages through alchemy and generates a matching list of sentiment scores/sentiment types

---
+++ msg_alchemize.py
@@ -0,0 +1,13 @@
+from alchemyapi import AlchemyAPI
+alchemyapi = AlchemyAPI()
+
+## Takes in a list of messages in text and returns a list of the sentiment types and scores calculated by Alchemy in the same order as the original list.
+def calculate_sentiments(messages):
+	responses = []
+	for message in messages:
+		response = alchemyapi.sentiment("text", message)
+		if response["docSentiment"]["type"] != "neutral":
+			responses.append((response["docSentiment"]["type"], response["docSentiment"]["score"]))
+		else: 
+			responses.append((response["docSentiment"]["type"], 0))
+	return responses
